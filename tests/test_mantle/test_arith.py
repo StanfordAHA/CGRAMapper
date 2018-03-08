6cd58abc55296c8f687ffdf24487b2bea22df13f
Lenny Truong <lenny@stanford.edu>
2018-03-07 22:54:02 -0800

    Add failing sub test

--- tests/test_mantle/test_arith.py
+++ tests/test_mantle/test_arith.py
@@ -3,18 +3,25 @@ import os
 os.environ["MANTLE"] = "coreir"
 import magma as m
 from magma.testing import check_files_equal
-from mantle.coreir.arith import DefineAdd, DefineSub, DefineNegate, DefineASR
+import mantle
+from mantle.coreir.arith import DefineAdd, DefineSub
 from magma.testing.newfunction import testvectors as function_test
 from magma.simulator.python_simulator import testvectors as simulator_test
 
+primitives = ["DefineAdd", "DefineSub"]
 
-def test_add():
+def pytest_generate_tests(metafunc):
+    if 'primitive' in metafunc.fixturenames:
+        metafunc.parametrize("primitive", primitives)
+
+def test_primitive(primitive):
+    prim = getattr(mantle.coreir, primitive)
     width = 16
-    m.compile("build/test_add16", DefineAdd(width), output="coreir")
+    m.compile(f"build/test_{primitive}16", prim(width), output="coreir")
     assert check_files_equal(__file__,
-            "build/test_add16.json", "gold/test_add16.json")
+            f"build/test_{primitive}16.json", f"gold/test_{primitive}16.json")
 
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    assert not delegator.run("../../bin/cgra-mapper build/test_add16.json build/test_add16_mapped.json", cwd=dir_path).return_code
+    assert not delegator.run(f"../../bin/cgra-mapper build/test_{primitive}16.json build/test_{primitive}16_mapped.json", cwd=dir_path).return_code
     assert check_files_equal(__file__,
-            "build/test_add16_mapped.json", "gold/test_add16_mapped.json")
+            f"build/test_{primitive}16_mapped.json", f"gold/test_{primitive}16_mapped.json")
