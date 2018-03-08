66dddcf07c4510b2e7741d928c159f3772fdc7bd
Ross <rdaly525@stanford.edu>
2018-03-08 08:09:29 -0800

    updated install script

--- .travis/install_coreir.sh
+++ .travis/install_coreir.sh
@@ -3,7 +3,7 @@
 set -e
 
 pip install -r requirements.txt
-if [ "$TRAVIS_BRANCH" != "master" ]; then
+if [ "$TRAVIS_BRANCH" != "master" || $TRAVIS_PULL_REQUEST]; then
     git clone -b dev https://github.com/rdaly525/coreir.git;
     cd coreir;
     make -j
@@ -15,7 +15,7 @@ else
     mkdir coreir_release 
     tar -xf coreir.tar.gz -C coreir_release --strip-components 1;
     cd coreir_release;
-    make install
+    sudo make install
     cd ..
     pip install coreir;
 fi
